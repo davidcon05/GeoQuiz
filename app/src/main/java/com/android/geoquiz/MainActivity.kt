94a094814ba34e099daf395b0f58582eb9b21ba5
davidcontreras <david.contreras@willowtreeapps.com>
2022-06-24 15:25:46 -0400

    didn't push everything

---
+++ app/src/main/java/com/android/geoquiz/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.android.geoquiz
+
+/*
+    Activity is a class within the Android SDK. We need an instance of this class
+    in order to enable a UI.
+
+    Subclasses of Activity are needed to implement functionality. A simple app
+    like GeoQuiz needs only one activity, MainActivity. This is written in XML
+ */
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
