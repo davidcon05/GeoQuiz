355594b5b358daa6bc3ea12b37a29f4bfc831c1c
davidcontreras <david.contreras@willowtreeapps.com>
2022-06-24 16:18:42 -0400

    completed UI

--- app/src/main/java/com/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/android/geoquiz/MainActivity.kt
@@ -8,10 +8,11 @@ package com.android.geoquiz
     like GeoQuiz needs only one activity, MainActivity. This is written in XML
  */
 
-import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity // compatibility support for older Androids
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    // inflates a layout and puts it on screen
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
