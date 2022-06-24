82038216f67b8639e5a74ca15331007859248c02
davidcontreras <david.contreras@willowtreeapps.com>
2022-06-24 17:31:10 -0400

    implemented toast function. Toast gravity cannot be changed for Android 11+

--- app/src/main/java/com/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/android/geoquiz/MainActivity.kt
@@ -10,11 +10,39 @@ package com.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity // compatibility support for older Androids
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    // declare widgets
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    fun topToast(stringId: Int, timeout: Int) {
+        val toast = Toast.makeText(this, stringId, timeout)
+        toast.setGravity(Gravity.TOP, 250, 250)
+        toast.show()
+    }
+
     // inflates a layout and puts it on screen
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.trueBtn)
+        falseButton = findViewById(R.id.falseBtn)
+
+        trueButton.setOnClickListener { view: View ->
+            topToast(R.string.correct, Toast.LENGTH_SHORT)
+
+        }
+
+        falseButton.setOnClickListener { view: View ->
+            topToast(R.string.incorrect, Toast.LENGTH_SHORT)
+        }
+
+
     }
 }
