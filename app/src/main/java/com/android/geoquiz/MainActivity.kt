1e9f1f2f067cb5256e816070a465974aec0db55e
davidcontreras <david.contreras@willowtreeapps.com>
2022-06-27 14:25:43 -0400

    implemented onclick listener to question text

--- app/src/main/java/com/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/android/geoquiz/MainActivity.kt
@@ -61,6 +61,11 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
+        questionTextView.setOnClickListener {
+            currentIndex = ( currentIndex + 1 ) % questionBank.size
+            updateQuestion()
+        }
+
         updateQuestion()
     }
 
